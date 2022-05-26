7889e8bcea69f548aa28df79379e087b3e373c2b
AlejaCalva <calvaalejandra19@gmail.com>
2022-05-25 21:03:47 -0500

    script del conteo de filas.
    Con este script vamos a saber el número de filas que tiene el n1.txt

--- Saavedra2013/netsize.sh
+++ Saavedra2013/netsize.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "Ibeth Calva,Análisis de datos simple"
+echo "Ibeth Calva, Análisis de datos simple"
 echo $1
-echo "Number of rows:"
+echo "Número de filas:"
 cat $1 | wc -l
-echo "Number of columns:"
-head -n 1 $1 | tr -d ' ' | tr -d ' ' | tr -d '\n' | wc -c
+
